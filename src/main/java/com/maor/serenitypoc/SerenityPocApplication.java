74c8ce060c615165cb0db9fffe81b2e15b07be39
Maor <maor.m1@gmail.com>
2021-10-06 15:04:45 +0300

    ASCII for "Started"

--- src/main/java/com/maor/serenitypoc/SerenityPocApplication.java
+++ src/main/java/com/maor/serenitypoc/SerenityPocApplication.java
@@ -8,6 +8,13 @@ public class SerenityPocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SerenityPocApplication.class, args);
+		System.out.println("\n" +
+				"  _____ _______       _____ _______ ______ _____  \n" +
+				" / ____|__   __|/\\   |  __ \\__   __|  ____|  __ \\ \n" +
+				"| (___    | |  /  \\  | |__) | | |  | |__  | |  | |\n" +
+				" \\___ \\   | | / /\\ \\ |  _  /  | |  |  __| | |  | |\n" +
+				" ____) |  | |/ ____ \\| | \\ \\  | |  | |____| |__| |\n" +
+				"|_____/   |_/_/    \\_\\_|  \\_\\ |_|  |______|_____/ \n");
 	}
 
 }
