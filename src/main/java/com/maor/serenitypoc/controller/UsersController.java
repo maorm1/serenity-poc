b6bffb995d5f07047b4a958058baed55f69b8206
Maor <maor.m1@gmail.com>
2021-10-07 22:48:00 +0300

    users controller

---
+++ src/main/java/com/maor/serenitypoc/controller/UsersController.java
@@ -0,0 +1,23 @@
+package com.maor.serenitypoc.controller;
+
+import com.maor.serenitypoc.data.User;
+import com.maor.serenitypoc.repo.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UsersController {
+
+	private final UsersRepository usersRepository;
+
+	@GetMapping()
+	public List<User> getUsers() {
+		return usersRepository.getAllUsers();
+	}
+}
