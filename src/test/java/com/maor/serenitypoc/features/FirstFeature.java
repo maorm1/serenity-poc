3dc624895c9c701e5487cfc75625c5b641092e89
Maor <maor.m1@gmail.com>
2021-10-09 21:15:02 +0300

    planned serenity test

--- src/test/java/com/maor/serenitypoc/features/FirstFeature.java
+++ src/test/java/com/maor/serenitypoc/features/FirstFeature.java
@@ -13,9 +13,31 @@ import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeT
 @RunWith(SerenityRunner.class)
 public class FirstFeature {
 
+	private String featureUrl = "http://localhost:8080/feature";
 	private String testUrl = "https://reqres.in/api";
 
-	private Actor bob = Actor.named("Bob the First");
+	private Actor bob = Actor.named("Bob the First"); // Has "use feature" user-right enabled
+	private Actor bill = Actor.named("Bill the Second"); // Has "use feature" user-right disabled
+
+	@Test
+
+	public void whatINeedToTestPositive() {
+		// given that bob can call an api at featureUrl
+		// and that bob has the "use feature" user-right enabled
+
+		// when bob attempts to get feature
+
+		// then bob should see that response status 200
+	}
+
+	public void whatINeedToTestNegative() {
+		// given that bill can call an api at featureUrl
+		// and that bill DOES NOT have the "use feature" user-right enabled
+
+		// when bill attempts to get feature
+
+		// then bill should see that response status is not 200
+	}
 
 	@Test
 	public void sanityTest() {
